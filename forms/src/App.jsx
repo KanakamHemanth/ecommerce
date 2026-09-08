2d37642cd965984a581add293e5f9dff0ee52baa
hemanthroot <hemanthhemanth4004@gmail.com>
2026-09-08 10:51:19 +0530

    Move navbar into components

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './Components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
